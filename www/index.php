199ee24ec9ea943a33fd664f15a35129769f1773
Errol Kelvin Tolentino <errolkelvintolentino@gmail.com>
2026-01-24 17:15:15 +0800

    Update index.php

--- www/index.php
+++ www/index.php
@@ -1,20 +1,3 @@
 <?php
-header('Content-Type: application/json');
-header('X-Content-Type-Options: nosniff');
-
-if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
-    http_response_code(405);
-    echo json_encode([
-        'success' => false,
-        'message' => 'Method not allowed'
-    ]);
-    exit;
-}
-
-echo json_encode([
-    'success' => true,
-    'service' => 'Inaagapay API',
-    'status' => 'running',
-    'version' => '1.0',
-    'timestamp' => date('Y-m-d H:i:s')
-]);
+header("Location: /admin");
+exit;
