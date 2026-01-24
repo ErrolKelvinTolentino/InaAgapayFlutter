083b72d6d186817436c70df780d960ec06c1054d
Errol Kelvin Tolentino <errolkelvintolentino@gmail.com>
2026-01-24 17:56:24 +0800

    Refractor
    refractor mailer.php and forgot_password.php

--- www/mailer.php
+++ www/mailer.php
@@ -2,11 +2,12 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require 'PHPMailer/src/Exception.php';
-require 'PHPMailer/src/PHPMailer.php';
-require 'PHPMailer/src/SMTP.php';
+require __DIR__ . '/PHPMailer/src/Exception.php';
+require __DIR__ . '/PHPMailer/src/PHPMailer.php';
+require __DIR__ . '/PHPMailer/src/SMTP.php';
 
-function sendMail($to, $subject, $message) {
+function sendMail($to, $subject, $message)
+{
     $mail = new PHPMailer(true);
 
     try {
